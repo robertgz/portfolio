ed2a4dfafeed7779cab5c9b26d510b41d1101f0e
Robert <robertgz@users.noreply.github.com>
2018-12-05 15:20:52 -0800

    added Projects component

---
+++ src/components/Projects.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12 sm10 offset-sm1 md6 offset-md0
+      v-for="item in samples"
+      v-bind:key="item.id" >
+      <v-card href height="100%" >
+        <v-img
+          :src=item.imageURL
+          class="my-3"
+          height="200">
+        </v-img>
+        <v-card-title primary-title>
+          <span class="headline">{{item.title}}</span>
+          <div v-html="item.text">{{item.text}}</div>
+          <ul>
+            <li v-for="bullet in item.bullets"
+              v-bind:key="bullet.id">
+              {{ bullet.text }}
+            </li>
+          </ul>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn :href="item.locationURL" >View</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+
+export default {
+  data () {
+    return {
+      samples: [
+        {
+          id: 0, 
+          imageURL: require('../assets/qwert_thumb.png'),
+          title: "Programing Example - 3D Spinning Shapes",
+          text: "Pressing any of the letters <strong>q, w, e, r, t</strong> on the keyboard causes a labeled cube to rotate in one direction. Pressing the letters <strong>a, s, d, f, g</strong> causes a cube to rotate in the opposite direction.",
+          bullets: [
+            { id: 0, text: "Uses three.js library and Javascript"},
+            { id: 1, text: "Shapes modeled in Blender"},
+            { id: 2, text: "Git source:"},
+            { id: 3, text: "Created: November 2018"},
+          ],
+          locationURL: "#"
+        },
+        {
+          id: 1, 
+          imageURL: require('../assets/cad_at_swc.png'),
+          title: "College Program Webpage", 
+          text: "Webpage listing courses, certificates, degrees, and schedules for the Computer Aided Design and Drafting program at Southwestern College in Chula Vista, CA.",
+          bullets: [
+            { id: 0, text: "Uses Bootstrap, HTML, CSS"},
+            { id: 1, text: "Git source: "},
+            { id: 2, text: "Designed: Fall 2016"},
+          ],
+          locationURL: "https://dept.swccd.edu/cad/demo/"
+        }
+      ]
+    }
+  }
+}
+</script>
