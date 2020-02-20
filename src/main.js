e78363f2ea3fb62b941b7c2f81b3440ba550091e
Robert <robertgz@users.noreply.github.com>
2020-02-19 17:59:31 -0800

    Add tag manager (#12)
    * updated vue cli
    
    * Added VueGtm

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import VueGtm from 'vue-gtm';
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtm, {
+  id: '', // Your GTM single container ID or array of container ids ['GTM-xxxxxxx', 'GTM-yyyyyyy']
+  // queryParams: { // Add url query string when load gtm.js with GTM ID (optional)
+  //   gtm_auth:'AB7cDEf3GHIjkl-MnOP8qr',
+  //   gtm_preview:'env-4',
+  //   gtm_cookies_win:'x'
+  // },
+  enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+  debug: true, // Whether or not display console logs debugs (optional)
+  loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional) 
+});
 
 new Vue({
   render: h => h(App),
